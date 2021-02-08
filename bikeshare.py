a4eccedab901339c45b785af671932d990b390d9
Itsaya Nakput <itsaya.nakput@gmail.com>
2021-02-08 10:56:48 +0100

    refactor: Refactoring production code
    git commit -m “refactor:Refactoring line 149”

--- bikeshare.py
+++ bikeshare.py
@@ -146,7 +146,7 @@ def user_stats(df):
     start_time = time.time()
 
    # TO DO: Display counts of user types
-    print("Here are the counts of various user types:")
+    print("Here are the counts for different user types:")
     user_types = df['User Type'].value_counts()
     print(user_types)
 
